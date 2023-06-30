980d1ef81b401923e12653b95047a7159455e469
Casey <leptone55@gmail.com>
2023-06-30 15:45:53 -0700

    draw frets

--- src/app/components/Guitar.tsx
+++ src/app/components/Guitar.tsx
@@ -3,8 +3,14 @@
 import { useEffect, useRef } from 'react'
 
 const NUM_STRINGS = 6
+const NUM_FRET_WIRES = 12
+
 const STRING_WIDTH = 2
+const FRET_WIRE_WIDTH = 6
+const NUT_WIDTH = 10
+
 const X_PADDING = 50
+const TOP_PADDING = 50
 
 type CanvasDimensions = {
   width: number
@@ -13,6 +19,26 @@ type CanvasDimensions = {
 
 type CanvasContext = CanvasDimensions & { ctx: CanvasRenderingContext2D }
 
+function drawFrets({ ctx, height, width }: CanvasContext) {
+  const fretSpacing = (height - TOP_PADDING) / NUM_FRET_WIRES
+  ctx.lineWidth = NUT_WIDTH
+  ctx.strokeStyle = '#000000'
+  ctx.beginPath()
+  ctx.moveTo(X_PADDING - STRING_WIDTH / 2, TOP_PADDING)
+  ctx.lineTo(width - X_PADDING + STRING_WIDTH / 2, TOP_PADDING)
+  ctx.stroke()
+
+  ctx.lineWidth = FRET_WIRE_WIDTH
+  ctx.strokeStyle = '#000000'
+  for (let i = 0; i < NUM_FRET_WIRES; i++) {
+    const y = TOP_PADDING + i * fretSpacing
+    ctx.beginPath()
+    ctx.moveTo(X_PADDING, y)
+    ctx.lineTo(width - X_PADDING, y)
+    ctx.stroke()
+  }
+}
+
 function drawStrings({ ctx, height, width }: CanvasContext) {
   const stringSpacing = (width - 2 * X_PADDING) / (NUM_STRINGS - 1)
   ctx.lineWidth = STRING_WIDTH
@@ -20,9 +46,8 @@ function drawStrings({ ctx, height, width }: CanvasContext) {
 
   for (let i = 0; i < NUM_STRINGS; i++) {
     const x = X_PADDING + i * stringSpacing
-
     ctx.beginPath()
-    ctx.moveTo(x, 0)
+    ctx.moveTo(x, TOP_PADDING)
     ctx.lineTo(x, height)
     ctx.stroke()
   }
@@ -31,6 +56,7 @@ function drawStrings({ ctx, height, width }: CanvasContext) {
 function paintCanvas({ ctx, height, width }: CanvasContext) {
   ctx.clearRect(0, 0, width, height)
   drawStrings({ ctx, width, height })
+  drawFrets({ ctx, width, height })
 }
 
 export default function Guitar() {
